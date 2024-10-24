8e8d048bd66e18109aa974a3a0faaaf20523df83
Prios <kanaev.i.v@yandex.ru>
2024-10-24 17:17:16 +0300

    Change Spring

---
+++ 7-Java-Spring/15-Homework5/homework5/src/main/java/org/example/service/PersonService.java
@@ -0,0 +1,34 @@
+package org.example.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.example.model.Person;
+import org.example.repository.PersonRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PersonService {
+    @Autowired
+    private PersonRepository repository;
+    public List<Person> getAllPersons(){
+        repository.save(new Person(null, "Nick", "nick@mail.ru"));
+        repository.save(new Person(null, "John", "john@mail.ru"));
+        repository.save(new Person(null, "Kate", "kate@mail.ru"));
+        return repository.findAll();
+    }
+
+
+    public Person getPersonById(Long id){
+        return repository.findById(id).get();
+    }
+    public Person create(Person person){
+        return repository.save(person);
+    }
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
+}
