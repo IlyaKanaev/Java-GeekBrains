b1a3f40eb7825636cc56d7dddbff0404bde858b2
IlyaKanaev <144250935+IlyaKanaev@users.noreply.github.com>
2024-10-27 22:34:39 +0300

    Delete 7-Java-Spring/04-Spring MVC-Thymeleaf/Homework4-v2.1.golikov.evgen directory
    wrong again

--- 7-Java-Spring/04-Spring MVC-Thymeleaf/Homework4-v2.1.golikov.evgen/src/main/java/ru/maliutin/diary_app/repositories/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.maliutin.diary_app.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.maliutin.diary_app.models.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
