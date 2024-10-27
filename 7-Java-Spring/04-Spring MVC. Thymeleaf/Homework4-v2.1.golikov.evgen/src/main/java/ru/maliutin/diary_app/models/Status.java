0bf37b68d70c4e854ff8e2d373b5dabf2a365a3d
IlyaKanaev <144250935+IlyaKanaev@users.noreply.github.com>
2024-10-27 22:31:08 +0300

    Delete 7-Java-Spring/04-Spring MVC. Thymeleaf/Homework4-v2.1.golikov.evgen directory
    wrong

--- 7-Java-Spring/04-Spring MVC. Thymeleaf/Homework4-v2.1.golikov.evgen/src/main/java/ru/maliutin/diary_app/models/Status.java
+++
@@ -1,9 +0,0 @@
-package ru.maliutin.diary_app.models;
-
-/**
- * Статусы задач.
- * Сделать, выполнена, истекает, просрочена.
- */
-public enum Status {
-    TO_DO, COMPLETED, EXPIRE, OVERLAY
-}
--- 7-Java-Spring/04-Spring MVC. Thymeleaf/Homework4-v2.1.golikov.evgen/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS task_table
-(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title varchar(255) NOT NULL,
-    description varchar(255) NOT NULL,
-    lead_time timestamp not null,
-    status varchar(50) not null
-    );
