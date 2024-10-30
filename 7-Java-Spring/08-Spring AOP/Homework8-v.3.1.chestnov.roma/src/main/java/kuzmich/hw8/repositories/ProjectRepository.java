c26cfdec8aefafee784271c08135e64452c81393
Prios <kanaev.i.v@yandex.ru>
2024-10-30 09:01:38 +0300

    Add spring aop

---
+++ 7-Java-Spring/08-Spring AOP/Homework8-v.3.1.chestnov.roma/src/main/java/kuzmich/hw8/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package kuzmich.hw8.repositories;
+
+import kuzmich.hw8.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+}
---
+++ 7-Java-Spring/08-Spring AOP/Homework8-v.3.1.chestnov.roma/src/main/java/kuzmich/hw8/services/ProxyTimesheetService.java
@@ -0,0 +1,29 @@
+package kuzmich.hw8.services;
+
+import kuzmich.hw8.model.Timesheet;
+
+public class ProxyTimesheetService extends TimesheetService {
+
+    private final TimesheetService original;
+
+    public ProxyTimesheetService(TimesheetService original) {
+        super(original);
+        this.original = original;
+    }
+
+    @Override
+    public Optional<Timesheet> original.getById(Long id) {
+        //BEFORE
+        Optional<Timesheet> result = null;
+        try {
+            result = super.getById(id);
+            //AFTER RETURNING
+        } catch (Throwable e) {
+            //AFTER THROWING
+            throw e;
+        } finally {
+            //AFTER
+            return result;
+        }
+    }
+}
