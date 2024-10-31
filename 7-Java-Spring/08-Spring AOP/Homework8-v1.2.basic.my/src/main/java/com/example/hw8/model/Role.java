adf21c7ff102cd3533098470db81a9bfb4c1d5ae
Prios <kanaev.i.v@yandex.ru>
2024-10-31 18:27:45 +0300

    Add my hw8 spring

---
+++ 7-Java-Spring/08-Spring AOP/Homework8-v1.2.basic.my/src/main/java/com/example/hw8/model/Role.java
@@ -0,0 +1,18 @@
+package com.example.hw8.model;
+
+import jakarta.persistence.*;
+
+public enum Role {
+
+    ADMIN("admin"), USER("user");
+
+    private final String name;
+
+    Role(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
