f26182a7bc821937f9ae1a58297cff5a4fd7ef84
Prios <kanaev.i.v@yandex.ru>
2024-11-11 17:30:06 +0300

    Add spring actuator

---
+++ 7-Java-Spring/11-Spring-Actuator/Homework11-v3.3.chestnov.roma/timesheet-page/src/test/java/kuzmich/hw9/Hw9ApplicationTests.java
@@ -0,0 +1,14 @@
+package kuzmich.hw9;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Hw9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
