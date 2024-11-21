b053fcd03016d634e08d9fb4c3fb7cda4d6a7529
Prios <kanaev.i.v@yandex.ru>
2024-11-21 15:17:56 +0300

    add hw1-my & seminar1 in 4-Java-Core/01-Platform

---
+++ 4-Java-Core/01-Platform/Homework1-my/mydocumentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.gb.safronov","l":"Decorator"},{"p":"ru.gb.safronov","l":"Main"},{"p":"ru.gb.safronov","l":"OtherClass"}]
